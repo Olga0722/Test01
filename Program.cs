b3b625f536151031a30eef2e9a32f412040ac7b3
olga <o.v.matvienko@yandex.ru>
2022-09-20 19:40:07 +0300

    добавлен файл Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+void ShortPartArray (string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
